417e372d2dadbd771f88db14f54bb3394b4cd877
saipranavam <245123737142@mvsrec.edu.in>
2026-02-19 10:04:11 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,45 +1,11 @@
-def add(x, y):
-   """This function adds two numbers"""
-
-   return x + y
-
-def subtract(x, y):
-   """This function subtracts two numbers"""
-
-   return x - y
-
-def multiply(x, y):
-   """This function multiplies two numbers"""
-
-   return x * y
-
-def divide(x, y):
-   """This function divides two numbers"""
-
-   return x / y
-
-# take input from the user
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
-
-choice = input("Enter choice(1/2/3/4):")
-
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-
-if choice == '1':
-   print(num1,"+",num2,"=", add(num1,num2))
-
-elif choice == '2':
-   print(num1,"-",num2,"=", subtract(num1,num2))
-
-elif choice == '3':
-   print(num1,"*",num2,"=", multiply(num1,num2))
-
-elif choice == '4':
-   print(num1,"/",num2,"=", divide(num1,num2))
-else:
-   print("Invalid input")
+import sys
+n1=int(sys.argv[1])
+n2=int(sys.args[2])
+r1=n1+n2
+r2=n1-n2
+r3=n1*n2
+r4=n1/n2
+print("Addition:",r1)
+print("Sub",r2)
+print("Mul",r3)
+print("Div",r4)
