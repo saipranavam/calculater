b29f34537f1c984f7ba91297551a07ddb2e37737
saipranavam <245123737142@mvsrec.edu.in>
2026-02-19 11:15:52 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,11 +1,11 @@
 import sys
 n1=int(sys.argv[1])
 n2=int(sys.argv[2])
-r6=n1+n2
+r7=n1+n2
 r2=n1-n2
 r3=n1*n2
 r4=n1/n2
-print("Addition:",r6)
+print("Addition:",r7)
 print("Sub",r2)
 print("Mul",r3)
 print("Div",r4)
